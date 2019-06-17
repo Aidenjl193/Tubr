49a33ba7b753208583b7426fcc1e83d080003483
aidenjl193 <aidenjl193@gmail.com>
2019-06-17 14:04:19 +0100

    stuff

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,19 @@
 class Line < ApplicationRecord
+  has_many :station_lines
+  has_many :stations, through: :station_lines
+  
+  def station_line_names=(names)
+    names.each do |name|
+      station = Station.find_by(name: name)
+      if(station)
+        self.station_lines.build(station_id: station.id)
+      end
+    end
+  end
+
+  def path_coords
+    self.station_lines.map do |station_line|
+      {x: station_line.station.x, y: station_line.station.y}
+    end
+  end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -5,84 +5,12 @@ const circleStyle = {
 	strokeWidth: 4
 }
 
-const lines = [
-	{
-		name: "circle",
-		color: "#FFCC00",
-		routes: [
-			{
-				stops: [
-					"Wood Lane",
-					"Latimer Road",
-					"Ladbroke Grove",
-					"Westbourne Park",
-					"Royal Oak",
-					"Paddington",
-					"Edgware Road",
-					"Baker Street",
-					"Great Portland Street",
-					"Euston Square",
-					"King’s Cross St Pancras",
-					"Farringdon",
-					"Barbican",
-					"Moorgate",
-					"Liverpool Street",
-					"Aldgate",
-					"Tower Hill Fenchurch Street"
-				]
-			}
-		]
-	},
-	{
-		name: "northern",
-		color: "black",
-		routes: [
-			{
-				stops: [
-					"Angel",
-					"Old Street",
-					"Moorgate",
-					"Bank",
-					"London Bridge"
-				]
-			}
-		]
-	},
-	{
-		name: "metropolitan",
-		color: "#660066",
-		routes: [
-			{
-				stops: [
-					"Aldgate",
-					"Liverpool Street",
-					"Moorgate",
-					"Barbican",
-					"Farringdon",
-					"King’s Cross St Pancras",
-					"Euston Square",
-					"Great Portland Street",
-					"Baker Street",
-					"Finchley Road",
-					"Wembley Park",
-					"Preston Road"
-				]
-			}
-		]
-	}
-]
 
-fetch("https://api.myjson.com/bins/6rccl")
+fetch("http://localhost:3000/lines/452")
 	.then(resp => resp.json())
 	.then(json => {
-		let stops = json
-		
-		for(let i = 0; i < stops.length; i++){
-			if(stops[i].pos == undefined || stops[i].x == undefined || stops[i].y == undefined) {
-				console.log(stops[i]);
-				stops.splice(i, 1);
-			}
-		}
+		let path_coords = json.path_coords
+		console.log(path_coords);
 		
 		var x = d3.scaleLinear();
 
@@ -127,64 +55,30 @@ fetch("https://api.myjson.com/bins/6rccl")
 			return (1-amt)*start+amt*end
 		}
 
-		function generateLineData(line) {
-			let lineCoords = [];
-			// populate initial coords
-			line.routes[0].stops.forEach((stationName) => {
-				stop =  stops.find((stop) => stop.name == stationName);
-				if(stop != undefined) {
-					const {x, y} = stop;
-					lineCoords.push({x, y})
-				}
-			});
-			
-			let lineCoordsSmoothed = [];
-			//smooth
-			for(let i = 0; i < lineCoords.length; ++i) {
-				coords = lineCoords[i];
-				lineCoordsSmoothed.push(coords);
-				if(lineCoords[i + 1]) {
-					coords2 = lineCoords[i + 1]
-					distance = Math.sqrt((coords.x - coords2.x) * (coords.x - coords2.x) + (coords.y - coords2.y) * (coords.y - coords2.y));
-					if(distance > 40) {
-						lineCoordsSmoothed.push({x: lerp(coords.x, coords2.x, 0.45), y: coords.y});
-						lineCoordsSmoothed.push({x: lerp(coords2.x, coords.x, 0.45), y: lerp(coords.y, coords2.y, 0.15)});
-						lineCoordsSmoothed.push(coords2);
-					} else {
-						lineCoordsSmoothed.push(coords2);
-					}
-				}
-			}
-			
-			return lineCoordsSmoothed;
-		}
-
-		lineData = generateLineData(lines[0]);
-
 		let lineFunction = d3.line()
             .x(d => d.x)
             .y(d => d.y)
             .curve(d3.curveBasis);
 
 		let lineGraphs = svgContainer.selectAll("path")
-            .data(lines)
+            .data(path_coords)
             .enter()
             .append("path")
-            .attr("d", d => lineFunction(generateLineData(d)))
-            .attr("stroke", d => d.color)
+            .attr("d", d => lineFunction(path_coords))
+            .attr("stroke", d => json.color)
             .attr("stroke-width", "10px")
             .attr("fill", "none");                         
 
 		//Render stations
 		const circles = svgContainer.selectAll("circle")
-              .data(stops)
+              .data(json.stations)
               .enter()
               .append("circle")
               .attr("cx", d => d.x)
               .attr("cy", d => d.y);
 		
 		const lables = svgContainer.selectAll("text")
-              .data(stops)
+              .data(json.stations)
               .enter()
               .append("text")
               .attr("x", d => d.x)
